5f7428658484f5c75f8ffacd52c7c6bda4ef8e9d
Max Okuneev <okuneev@gmail.com>
2019-11-28 11:31:28 +0700

    Initial Commit

---
+++ 18.Standart MVVM project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  18.Standart MVVM project
+//
+//  Created by Максим Окунеев on 11/28/19.
+//  Copyright © 2019 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
